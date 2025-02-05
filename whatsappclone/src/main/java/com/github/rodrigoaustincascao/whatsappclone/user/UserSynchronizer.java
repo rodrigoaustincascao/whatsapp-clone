a21a41c3cc694e971ab0ea85d9007869391697b1
t320790 <t320790@tjdft.jus.br>
2025-02-05 15:10:25 -0300

    Sincronizando Usuário com IDP

---
+++ whatsappclone/src/main/java/com/github/rodrigoaustincascao/whatsappclone/user/UserSynchronizer.java
@@ -0,0 +1,38 @@
+package com.github.rodrigoaustincascao.whatsappclone.user;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class UserSynchronizer {
+
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    
+    public void synchronizeWithIdp(Jwt token) {
+        log.info("Sincronizando usuários com IDP");
+        getUserEmail(token)
+                .ifPresent(userEmail ->{
+                    log.info("Sincronizando usuário que possui e-mail");
+                    Optional<User> optUser = userRepository.findByEmail(userEmail);
+                    User user = userMapper.fromTokenAttributes(token.getClaims());
+                    optUser.ifPresent(value-> user.setId(optUser.get().getId()));
+                    userRepository.save(user);
+                });
+    }
+
+    private Optional<String> getUserEmail(Jwt token){
+        Map<String, Object> attributes = token.getClaims();
+        if(attributes.containsKey("email")){
+            return Optional.of(attributes.get("email").toString());
+        }
+        return Optional.empty();
+    }
+}
