e2c1931338b4b444170f6fb52adbcd7169aeeb05
rodrigoaustincascao <t320790@tjdft.jus.br>
2025-02-21 12:06:07 +0000

    add devcontainer

--- whatsappclone/src/main/java/com/github/rodrigoaustincascao/whatsappclone/user/UserRepository.java
+++ whatsappclone/src/main/java/com/github/rodrigoaustincascao/whatsappclone/user/UserRepository.java
@@ -4,11 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-
 import java.util.List;
 import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, String> {
 
     @Query(name = UserConstants.FIND_USER_BY_EMAIL)
     Optional<User> findByEmail(@Param("email") String userEmail);
